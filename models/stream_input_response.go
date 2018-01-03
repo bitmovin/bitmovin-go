dac6cf06f7e3d0e7611323404f50668fc3b13389
Francisco Souza <fsouza@users.noreply.github.com>
2018-01-03 17:21:30 -0500

    models/stream_input: add omitempty to json tags

--- models/stream_input_response.go
+++ models/stream_input_response.go
@@ -3,45 +3,45 @@ package models
 import "github.com/bitmovin/bitmovin-go/bitmovintypes"
 
 type StreamInputData struct {
-	Result StreamInputResult `json:"result"`
+	Result StreamInputResult `json:"result,omitempty"`
 }
 
 type StreamInputResult struct {
-	FormatName   *string                `json:"formatName"`
-	StartTime    *float64               `json:"startTime"`
-	Duration     *float64               `json:"duration"`
-	Size         *int64                 `json:"size"`
-	Bitrate      *int64                 `json:"bitrate"`
-	AudioStreams []StreamInputAudio     `json:"audioStreams"`
-	VideoStreams []StreamInputVideo     `json:"videoStreams"`
-	Tags         map[string]interface{} `json:"tags"`
+	FormatName   *string                `json:"formatName,omitempty"`
+	StartTime    *float64               `json:"startTime,omitempty"`
+	Duration     *float64               `json:"duration,omitempty"`
+	Size         *int64                 `json:"size,omitempty"`
+	Bitrate      *int64                 `json:"bitrate,omitempty"`
+	AudioStreams []StreamInputAudio     `json:"audioStreams,omitempty"`
+	VideoStreams []StreamInputVideo     `json:"videoStreams,omitempty"`
+	Tags         map[string]interface{} `json:"tags,omitempty"`
 }
 
 type StreamInputAudio struct {
-	ID              *string  `json:"id"`
-	Position        *int64   `json:"position"`
-	Duration        *float64 `json:"duration"`
-	Codec           *string  `json:"codec"`
-	SampleRate      *int64   `json:"sampleRate"`
-	Bitrate         *int64   `json:"bitrate,string"`
-	ChannelFormat   *string  `json:"channelFormat"`
-	Language        *string  `json:"language"`
-	HearingImpaired *bool    `json:"hearingImpaired"`
+	ID              *string  `json:"id,omitempty"`
+	Position        *int64   `json:"position,omitempty"`
+	Duration        *float64 `json:"duration,omitempty"`
+	Codec           *string  `json:"codec,omitempty"`
+	SampleRate      *int64   `json:"sampleRate,omitempty"`
+	Bitrate         *int64   `json:"bitrate,string,omitempty"`
+	ChannelFormat   *string  `json:"channelFormat,omitempty"`
+	Language        *string  `json:"language,omitempty"`
+	HearingImpaired *bool    `json:"hearingImpaired,omitempty"`
 }
 
 type StreamInputVideo struct {
-	ID       *string  `json:"id"`
-	Position *int64   `json:"position"`
-	Duration *float64 `json:"duration"`
-	Codec    *string  `json:"codec"`
-	FPS      *string  `json:"fps"`
-	Bitrate  *int64   `json:"bitrate,string"`
-	Width    *int64   `json:"width"`
-	Height   *int64   `json:"height"`
+	ID       *string  `json:"id,omitempty"`
+	Position *int64   `json:"position,omitempty"`
+	Duration *float64 `json:"duration,omitempty"`
+	Codec    *string  `json:"codec,omitempty"`
+	FPS      *string  `json:"fps,omitempty"`
+	Bitrate  *int64   `json:"bitrate,string,omitempty"`
+	Width    *int64   `json:"width,omitempty"`
+	Height   *int64   `json:"height,omitempty"`
 }
 
 type StreamInputResponse struct {
-	RequestID *string                      `json:"requestId"`
-	Status    bitmovintypes.ResponseStatus `json:"status"`
-	Data      StreamInputData              `json:"data"`
+	RequestID *string                      `json:"requestId,omitempty"`
+	Status    bitmovintypes.ResponseStatus `json:"status,omitempty"`
+	Data      StreamInputData              `json:"data,omitempty"`
 }
