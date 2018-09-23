7fae2d3cb6fe1de77a470ebc9f119dae2ef26581
Matthew Lee <matt@matt-lee.sg>
2018-09-24 00:04:42 +0800

    Added Motion Estimation Method and SubMe to h264

---
+++ bitmovintypes/subme.go
@@ -0,0 +1,19 @@
+package bitmovintypes
+
+type SubMe string
+
+const (
+	SubMeFULLPEL  = `FULLPEL`
+	SubMeSAD      = `SAD`
+	SubMeSATD     = `SATD`
+	SubMeQPEL3    = `QPEL3`
+	SubMeQPEL4    = `QPEL4`
+	SubMeQPEL5    = `QPEL5`
+	SubMeRDIP     = `RD_IP`
+	SubMeRDALL    = `RD_ALL`
+	SubMeRDREFIP  = `RD_REF_IP`
+	SubMeRDREFALL = `RD_REF_ALL`
+	SubMeQPRD     = `QPRD`
+	SubMeFULLRD   = `FULL_RD`
+	SubMeDefault  = SubMeRDREFIP
+)
