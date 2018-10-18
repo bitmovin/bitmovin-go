aeb1f9f872c6ab57c8ccd55d22d44494bea713ab
Sebastian Burgstaller <sebastian.burgstaller@bitmovin.com>
2018-10-18 10:49:55 +0200

    Added encoding mode SINGLE_PASS, as the new standard mode is TWO_PASS

--- bitmovintypes/encoding_mode.go
+++ bitmovintypes/encoding_mode.go
@@ -3,7 +3,8 @@ package bitmovintypes
 type EncodingMode string
 
 const (
-	EncodingModeStandard  EncodingMode = "STANDARD"
-	EncodingModeTwoPass   EncodingMode = "TWO_PASS"
-	EncodingModeThreePass EncodingMode = "THREE_PASS"
+	EncodingModeStandard   EncodingMode = "STANDARD"
+	EncodingModeSinglePass EncodingMode = "SINGLE_PASS"
+	EncodingModeTwoPass    EncodingMode = "TWO_PASS"
+	EncodingModeThreePass  EncodingMode = "THREE_PASS"
 )
