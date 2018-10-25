f2450a3d784a28b801fdf7c1512f897160015713
Matthew Lee <matt@matt-lee.sg>
2018-10-25 12:15:11 +0800

    Added the Ability to set type of Fragmented MP4 Muxing (Smooth, DASH On Demand)

---
+++ bitmovintypes/fragmented_mp4_muxing_manifest_type.go
@@ -0,0 +1,8 @@
+package bitmovintypes
+
+type FragmentedMP4MuxingManifestType string
+
+const (
+	FragmentedMP4MuxingManifestTypeSmooth        FragmentedMP4MuxingManifestType = "SMOOTH"
+	FragmentedMP4MuxingManifestTypeDASHOnDemand  FragmentedMP4MuxingManifestType = "DASH_ON_DEMAND"
+)
