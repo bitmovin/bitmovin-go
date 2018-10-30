71f960ae52e972fe9c7d01342037b553009a37ff
Matthew Lee <matt@matt-lee.sg>
2018-10-30 14:29:57 +0800

    Added HLS Byte Range Support (Encoder v2.3.0+ only)

--- bitmovintypes/fragmented_mp4_muxing_manifest_type.go
+++ bitmovintypes/fragmented_mp4_muxing_manifest_type.go
@@ -5,4 +5,5 @@ type FragmentedMP4MuxingManifestType string
 const (
 	FragmentedMP4MuxingManifestTypeSmooth        FragmentedMP4MuxingManifestType = "SMOOTH"
 	FragmentedMP4MuxingManifestTypeDASHOnDemand  FragmentedMP4MuxingManifestType = "DASH_ON_DEMAND"
+	FragmentedMP4MuxingManifestTypeHlsByteRanges FragmentedMP4MuxingManifestType = "HLS_BYTE_RANGES"
 )
