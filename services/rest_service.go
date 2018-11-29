f2f8bcc93c0e86ba85af61c50493fac8578fd871
Matthew Lee <matt@matt-lee.sg>
2018-11-29 16:11:42 +0800

    Cosmetics

--- services/rest_service.go
+++ services/rest_service.go
@@ -36,6 +36,7 @@ func (r *RestService) Create(relativeURL string, input []byte) ([]byte, error) {
 	}
 	req.Header.Set("X-Api-Client", ClientName)
 	req.Header.Set("X-Api-Client-Version", Version)
+
 	resp, err := r.Bitmovin.HTTPClient.Do(req)
 	if err != nil {
 		return nil, err
