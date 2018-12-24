0030c1290bb34a2afaa72c46be014b2d0996d822
Matthew Lee <matt@matt-lee.sg>
2018-12-24 16:38:04 +0800

    Added PER_TITLE_TEMPLATE_FIXED_RESOLUTION

--- bitmovintypes/stream_mode.go
+++ bitmovintypes/stream_mode.go
@@ -3,7 +3,8 @@ package bitmovintypes
 type StreamMode string
 
 const (
-	StreamModeStandard         StreamMode = "STANDARD"
-	StreamModePerTitleTemplate StreamMode = "PER_TITLE_TEMPLATE"
-	StreamModePerTitleResult   StreamMode = "PER_TITLE_RESULT"
+	StreamModeStandard                        StreamMode = "STANDARD"
+	StreamModePerTitleTemplate                StreamMode = "PER_TITLE_TEMPLATE"
+	StreamModePerTitleResult                  StreamMode = "PER_TITLE_RESULT"
+	StreamModePerTitleTemplateFixedResolution StreamMode = "PER_TITLE_TEMPLATE_FIXED_RESOLUTION"
 )
