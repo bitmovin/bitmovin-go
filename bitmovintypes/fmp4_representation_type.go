57b79d41eb69b8da8be4238b478021acc06e41c2
Matthew Lee <matt@matt-lee.sg>
2019-04-25 22:35:24 +0800

    Add TEMPLATE_ADAPTATION_SET

--- bitmovintypes/fmp4_representation_type.go
+++ bitmovintypes/fmp4_representation_type.go
@@ -3,7 +3,8 @@ package bitmovintypes
 type FMP4RepresentationType string
 
 const (
-	FMP4RepresentationTypeTemplate FMP4RepresentationType = "TEMPLATE"
-	FMP4RepresentationTypeList     FMP4RepresentationType = "LIST"
-	FMP4RepresentationTypeTimeline FMP4RepresentationType = "TIMELINE"
+	FMP4RepresentationTypeTemplate              FMP4RepresentationType = "TEMPLATE"
+	FMP4RepresentationTypeList                  FMP4RepresentationType = "LIST"
+	FMP4RepresentationTypeTimeline              FMP4RepresentationType = "TIMELINE"
+	FMP4RepresentationTypeTemplateAdaptationSet FMP4RepresentationType = "TEMPLATE_ADAPTATION_SET"
 )
