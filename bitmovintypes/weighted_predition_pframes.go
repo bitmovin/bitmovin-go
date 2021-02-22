fd20f6ce2683ed38a699a5308c505c1d63e7bc6b
Roberto Amici <roberto.amici@gmail.com>
2021-02-22 13:47:11 +1100

    Bitmovin: new parameters for h264 codec

---
+++ bitmovintypes/weighted_predition_pframes.go
@@ -0,0 +1,9 @@
+package bitmovintypes
+
+type WeightedPredictionPFrames string
+
+const (
+	WeightedPredictionPFramesDisabled WeightedPredictionPFrames = "DISABLED"
+	WeightedPredictionPFramesSimple   WeightedPredictionPFrames = "SIMPLE"
+	WeightedPredictionPFramesSmart    WeightedPredictionPFrames = "SMART"
+)
